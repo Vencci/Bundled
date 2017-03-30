5bd6c82ce164cf38a8536a417d145d3c5915b1d3
sz117 <sz117@duke.edu>
2017-03-30 13:27:43 -0400

    shopplist cell added

---
+++ Bundled/ShoppingListCell.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingListCell.swift
+//  Bundled
+//
+//  Created by si zhang on 17/3/28.
+//  Copyright © 2017年 si zhang. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var number: UILabel!
+    
+}
