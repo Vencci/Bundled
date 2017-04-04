9f701f1c4d319a64eb16e4704ec8d6f9659c677f
cz90 <cz90@duke.edu>
2017-04-04 12:14:49 -0400

    delegate file modified

--- Bundled/AppDelegate1.swift
+++ Bundled/AppDelegate1.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  TabbarPages
+//  AppStore
 //
-//  Created by Wenqi Wang on 31/03/2017.
-//  Copyright © 2017 SixSix. All rights reserved.
+//  Created by Wenqi Wang on 24/03/2017.
+//  Copyright © 2017 Wenqi Wang. All rights reserved.
 //
 
 import UIKit
@@ -16,17 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        let layout = UICollectionViewFlowLayout()
-        window?.rootViewController = UINavigationController(rootViewController: PreparationController(collectionViewLayout: layout))
-        UINavigationBar.appearance().barTintColor = UIColor(red: 32/255, green: 150/255, blue: 230/255, alpha: 1)
         
-        //消除view间黑线
-        UINavigationBar.appearance().shadowImage = UIImage()
-        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        let layout = UICollectionViewFlowLayout()
+        let featuredAppsController = FeaturedBundlesController(collectionViewLayout: layout)
+        window?.rootViewController = UINavigationController(rootViewController:featuredAppsController)
         return true
     }
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
