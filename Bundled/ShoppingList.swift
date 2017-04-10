1cceb0cdfaf62f0b30e4d40831b5cf56fa85f73f
sz117 <sz117@duke.edu>
2017-04-09 23:47:49 -0400

    shopping modify

---
+++ Bundled/ShoppingList.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingList.swift
+//  AppStore
+//
+//  Created by Wenqi Wang on 06/04/2017.
+//  Copyright © 2017 Wenqi Wang. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingList: BaseCell {
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        backgroundColor = UIColor.red
+        
+    }
+}
