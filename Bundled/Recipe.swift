7f1b80487ab5c4f944ec00b3cfd931284f260fb2
cz90 <cz90@duke.edu>
2017-04-09 23:39:34 -0400

    recipe model

---
+++ Bundled/Recipe.swift
@@ -0,0 +1,39 @@
+//
+//  Recipe.swift
+//  TabbarPages
+//
+//  Created by Wenqi Wang on 06/04/2017.
+//  Copyright © 2017 SixSix. All rights reserved.
+//
+
+import UIKit
+
+class Recipe: NSObject {
+    var id: NSNumber
+    var name: String
+    var category: String
+    var preparationArray: [Preparation]
+    var imageName: String
+    var price:Float
+    var cookTime: Int
+    var tag: [String]
+    var ingredients: [String:(Int, String)]
+    var steps: [(String, String)]
+    var details: String
+    
+    init(id: NSNumber, name: String, category: String, preparationArray: [Preparation], imageName: String, price: Float, cookTime: Int, tag: [String], ingredients: [String:(Int, String)],steps:[(String, String)], details: String) {
+        self.id = id
+        self.name = name
+        self.category = category
+        self.preparationArray = preparationArray
+        self.imageName = imageName
+        self.price = price
+        self.cookTime = cookTime
+        self.tag = tag
+        self.ingredients = ingredients
+        self.steps = steps
+        self.details = details
+    }
+}
+
+
