633e3158002984755775d2e2f1c6f33107d20b72
sz117 <sz117@duke.edu>
2017-04-09 23:47:18 -0400

    review function

---
+++ Bundled/ReviewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ReviewCell.swift
+//  AppStore
+//
+//  Created by Wenqi Wang on 06/04/2017.
+//  Copyright © 2017 Wenqi Wang. All rights reserved.
+//
+
+import UIKit
+
+class ReviewCell: BaseCell {
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        backgroundColor = UIColor.brown
+        
+    }
+}
